5625d1adf65bbb0bfb5d8ac578bc2f72729bc7b5
wtait <wade.tait@gmail.com>
2019-01-11 18:45:26 -0500

    fixes missing return statement in test

--- test/dappToken.js
+++ test/dappToken.js
@@ -1,15 +1,17 @@
 var DappToken = artifacts.require("./dappToken.sol");
 
 contract('DappToken', function(accounts) {
-//tests here
-it('sets the total supply upon deployment', function() {
-    DappToken.deployed().then(function(instance){
-        tokenInstance = instance;
-        return tokenInstance.totalSupply();
-    }).then(function(totalSupply) {
-        assert.equal(totalSupply.toNumber(), 1000000, "Sets token total supply at one million");
-    });
-});
+
+    it('sets the total supply upon deployment', function() {
+        return DappToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.totalSupply();
+        }).then(function(totalSupply) {
+            console.log(totalSupply);
+            assert.equal(totalSupply.toNumber(), 1000000, "Sets token total supply at one million");
+        });
+    })
+
 
 
 })
