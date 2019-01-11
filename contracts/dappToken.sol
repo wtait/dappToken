c267d94ba56528604513d2e9433681064a204bd3
wtait <wade.tait@gmail.com>
2019-01-11 15:34:58 -0500

    totalSupply and dappToken constructor
    declare totalSupply as state variable. declare constructor function and set totalSupply equal to one million. conforms to erc20 standard.

--- contracts/dappToken.sol
+++ contracts/dappToken.sol
@@ -1,5 +1,10 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
-//contract here 
+
+    uint256 public totalSupply;
+    
+    constructor () public {
+        totalSupply = 1000000;
+    }
 }
