0649804cefcea384cbf2d7c1530279a0f4417130
wtait <wade.tait@gmail.com>
2019-01-11 18:01:09 -0500

    test that contract sets total supply to one million
    import contract using require statement then instantiate and deploy contract. because totalSupply is a state variable in the contract it comes with a getter function. Because javascript treats this as a big number (<BN>), we format with toNumber() method and, utilizing mocha/chai library, assert it to be equal to what we assigned to it in the contract (one million in this case).

--- test/dappToken.js
+++ test/dappToken.js
@@ -2,4 +2,14 @@ var DappToken = artifacts.require("./dappToken.sol");
 
 contract('DappToken', function(accounts) {
 //tests here
+it('sets the total supply upon deployment', function() {
+    DappToken.deployed().then(function(instance){
+        tokenInstance = instance;
+        return tokenInstance.totalSupply();
+    }).then(function(totalSupply) {
+        assert.equal(totalSupply.toNumber(), 1000000, "Sets token total supply at one million");
+    });
+});
+
+
 })
