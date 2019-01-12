6c55c9b35041e57106d92291f554cb02ed20e71b
wtait <wade.tait@gmail.com>
2019-01-12 15:53:31 -0500

    defines balanceOf mapping
    a mapping in Solidity is a key value store similar to a hash table or associative array in other languages. balanceOf will define and track the allocation of all tokens in our dapp.

--- contracts/dappToken.sol
+++ contracts/dappToken.sol
@@ -4,6 +4,8 @@ contract DappToken {
 
     uint256 public totalSupply;
 
+    mapping(address => uint256) public balanceOf;
+
     constructor (uint256 _initialSupply) public {
         totalSupply = _initialSupply;
     }
