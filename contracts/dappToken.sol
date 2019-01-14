c52ab5d2b4b356866930c25289e2b8bcab2240a8
wtait <wade.tait@gmail.com>
2019-01-14 11:48:00 -0500

    Adds token name and symbol
    creates two state variables in dappToken.sol; name and symbol, both public strings and currently hard coded to 'Dapps' and 'DAPP' respectively.
    Also describes test cases to verify correct assignment of name and symbol upon contract instantiation;

--- contracts/dappToken.sol
+++ contracts/dappToken.sol
@@ -2,6 +2,10 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
 
+    string public name = "Dapps";
+
+    string public symbol = "DAPP";
+
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
--- test/dappToken.js
+++ test/dappToken.js
@@ -1,16 +1,28 @@
 var DappToken = artifacts.require("./dappToken.sol");
 
-contract('DappToken', function(accounts) {
+contract('DappToken', function (accounts) {
 
-    it('sets the total supply upon deployment', function() {
-        return DappToken.deployed().then(function(instance){
+    it('initializes contract with the correct name', function() {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name) {
+            assert.equal(name, 'Dapps', 'is named Dapps');
+            return tokenInstance.symbol();
+        }).then(function(symbol) {
+            assert.equal(symbol, 'DAPP', 'symbol is DAPP');
+        })
+    });
+
+    it('sets the total supply upon deployment', function () {
+        return DappToken.deployed().then(function (instance) {
             tokenInstance = instance;
             return tokenInstance.totalSupply();
-        }).then(function(totalSupply) {
+        }).then(function (totalSupply) {
             console.log(totalSupply);
             assert.equal(totalSupply.toNumber(), 1000000, "Sets token total supply at one million");
             return tokenInstance.balanceOf(accounts[0]);
-        }).then(function(adminBalance) {
+        }).then(function (adminBalance) {
             assert.equal(adminBalance.toNumber(), 1000000, "Allocates total supply of initial coins to admin");
         });
     });
